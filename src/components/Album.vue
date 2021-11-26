f7a95ac5e9b0cbf6c19f5557c1e71332f1fdeb4d
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-26 15:16:02 +0100

    album component

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,7 +1,24 @@
 <template>
 
-  <div>
-    
+  <div class="row">
+
+    <div class="col text-center p-4 album-box ">
+
+      <div class="mx-auto album-img">
+        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="img">
+      </div>
+              
+      <div class="py-3 album-title">
+        <p>TITOLO</p>
+      </div>
+              
+      <div class="album-info">
+        <p class="ai-author">Autore</p>
+        <p class="ai-year">ANNO</p>
+      </div>
+
+    </div>
+
   </div>
 
 </template>
@@ -16,6 +33,31 @@ export default {
 
 @import "../assets/style/vars.scss";
 
+.album-box{
+  background-color: lighten($main-color, 30%);
+  margin: 100px 20px 0 20px;
 
+  .album-img{
+    width: 150px;
+    height: 150px;
+  }
+
+  .album-title{
+    font-size: 30px;
+    color: white;
+  }
+
+  .album-info{
+    color: grey;
+        
+    .ai-author{
+      font-size: 30px;
+    }
+
+    .ai-year{
+      font-size: 20px;
+    }
+  }
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,92 +1,13 @@
 <template>
   
   <main>
+
     <div class="container">
-      <div class="row">
-
-        <div class="col text-center p-4 album-box ">
-
-          <div class="mx-auto album-img">
-            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="img">
-          </div>
-              
-          <div class="py-3 album-title">
-            <p>TITOLO</p>
-          </div>
-              
-          <div class="album-info">
-            <p class="ai-author">Autore</p>
-            <p class="ai-year">ANNO</p>
-          </div>
-
-        </div>
-        <div class="col text-center p-4 album-box ">
-
-          <div class="mx-auto album-img">
-            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="img">
-          </div>
-              
-          <div class="py-3 album-title">
-            <p>TITOLO</p>
-          </div>
-              
-          <div class="album-info">
-            <p class="ai-author">Autore</p>
-            <p class="ai-year">ANNO</p>
-          </div>
-
-        </div>
-        <div class="col text-center p-4 album-box ">
-
-          <div class="mx-auto album-img">
-            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="img">
-          </div>
-              
-          <div class="py-3 album-title">
-            <p>TITOLO</p>
-          </div>
-              
-          <div class="album-info">
-            <p class="ai-author">Autore</p>
-            <p class="ai-year">ANNO</p>
-          </div>
-
-        </div>
-        <div class="col text-center p-4 album-box ">
-
-          <div class="mx-auto album-img">
-            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="img">
-          </div>
-              
-          <div class="py-3 album-title">
-            <p>TITOLO</p>
-          </div>
-              
-          <div class="album-info">
-            <p class="ai-author">Autore</p>
-            <p class="ai-year">ANNO</p>
-          </div>
-
-        </div>
-        <div class="col text-center p-4 album-box ">
-
-          <div class="mx-auto album-img">
-            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="img">
-          </div>
-              
-          <div class="py-3 album-title">
-            <p>TITOLO</p>
-          </div>
-              
-          <div class="album-info">
-            <p class="ai-author">Autore</p>
-            <p class="ai-year">ANNO</p>
-          </div>
-
-        </div>
-
-      </div>
+  
+      <Album />
+  
     </div>
+  
   </main>
 
 </template>
@@ -111,35 +32,6 @@ export default {
 main{
   background-color: $main-color;
   height: calc(100vh - 70px);
-
-  .album-box{
-    //width: calc((100% / 5) - 40px);
-    // height: 400px;
-    background-color: lighten($main-color, 30%);
-    margin: 100px 20px 0 20px;
-
-    .album-img{
-      width: 150px;
-      height: 150px;
-    }
-
-    .album-title{
-      font-size: 30px;
-      color: white;
-    }
-
-    .album-info{
-      color: grey;
-      
-      .ai-author{
-        font-size: 30px;
-      }
-
-      .ai-year{
-        font-size: 20px;
-      }
-    }
-  }
 }
 
 </style>
