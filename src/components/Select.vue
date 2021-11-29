06e73179a75addfc00b14436573a25e39042082a
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-29 15:54:46 +0100

    select album genre

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,28 +1,29 @@
 <template>
-  <div>
-    <select class="form-select af-select" aria-label="Default select example">
+  <div class="col-2">
+    <select class="form-select" aria-label="Default select example">
       <option selected>Seleziona il genere musicale</option>
-      <option value="1">Rock</option>
+      <option value="">{{album.genre}}</option>
+      <!-- <option value="1">Rock</option>
       <option value="2">Pop</option>
       <option value="3">Jazz</option>
-      <option value="4">Metal</option>
+      <option value="4">Metal</option> -->
     </select>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Select'
+  name: 'Select',
+
+  props:{
+    album: Object
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 
-@import "../assets/style/vars.scss";
+// @import "../assets/style/vars.scss";
 
-.af-select{
-    width: 300px !important;
-    height: 40px !important;
-  }
 
 </style>
