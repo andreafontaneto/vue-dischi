e9a4d3d22133482a610d646f8ef6f7abe568ce49
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-29 15:17:14 +0100

    select structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,19 @@
 <template>
   
   <header class="px-4">
+    
     <div class="py-3 logo">
       <img src="../assets/img/spotify-logo.png" alt="spotify-logo">
     </div>
+
+    <select class="form-select af-select" aria-label="Default select example">
+      <option selected>Seleziona il genere musicale</option>
+      <option value="1">Rock</option>
+      <option value="2">Pop</option>
+      <option value="3">Jazz</option>
+      <option value="4">Metal</option>
+    </select>
+
   </header>
 
 </template>
@@ -21,9 +31,17 @@ export default {
 header{
   height: 70px;
   background-color: $box-color;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   .logo{
     width: 40px;
+  }
+
+  .af-select{
+    width: 300px;
+    height: 40px;
   }
 }
 
